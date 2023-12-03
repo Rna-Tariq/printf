2e166799f09b60c32ea0c1d8a025e01c30d77a31
Rna341 <rna.tariq.aboouf@gmail.com>
2023-12-03 19:14:36 +0200

    tasks 0,1

--- _printf.c
+++ _printf.c
@@ -8,12 +8,62 @@
 
 int _printf(const char *format, ...)
 {
+	char buffer[MAX_BUF_SIZE];
+	int buffer_index = 0;
+	int char_count = 0;
+
 	va_list args;
 
 	va_start(args, format);
 
-	process_format2(format, args);
+	while (*format)
+	{
+		if (*format == '%' && format++)
+		{
+			switch (*format)
+			{
+				case 'c':
+				{
+					char ch = va_arg(args, int);
+
+					_putchar(ch, buffer, &buffer_index, &char_count);
+					break;
+				}
+				case 's':
+				{
+					const char *str = va_arg(args, const char *);
+
+					_puts(str, buffer, &buffer_index, &char_count);
+					break;
+				}
+				case 'd':
+				case 'i':
+				{
+					int value = va_arg(args, int);
+
+					_int_to_str(value, buffer, &buffer_index, &char_count);
+					break;
+				}
+				case '%':
+				{
+					_putchar('%', buffer, &buffer_index, &char_count);
+					break;
+				}
+				default:
+				{
+					_putchar(*format, buffer, &buffer_index, &char_count);
+					break;
+				}
+			}
+		} else
+			_putchar(*format, buffer, &buffer_index, &char_count);
+
+		format++;
+	}
+
+	write(STDOUT_FILENO, buffer, buffer_index);
 
 	va_end(args);
-	return (0);
+
+	return (char_count);
 }
