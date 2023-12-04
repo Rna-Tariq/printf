b7489dcaa271f93cb45b83cfb4995e0382fdf8ee
moutazmahmoud <moutaz.prof.egy@gmail.com>
2023-12-04 05:10:54 +0200

    fix: betty style 1

--- _printf.c
+++ _printf.c
@@ -8,66 +8,66 @@
 
 int _printf(const char *format, ...)
 {
-    char buffer[MAX_BUF_SIZE];
-    int buffer_index = 0;
-    int char_count = 0;
+char buffer[MAX_BUF_SIZE];
+int buffer_index = 0;
+int char_count = 0;
 
-    va_list args;
+va_list args;
 
-    va_start(args, format);
+va_start(args, format);
 
-    while (*format)
-    {
-        if (*format == '%' && format++)
-        {
-            switch (*format)
-            {
-                case 'c':
-                {
-                    char ch = va_arg(args, int);
+while (*format)
+{
+if (*format == '%' && format++)
+{
+switch (*format)
+{
+case 'c':
+{
+char ch = va_arg(args, int);
 
-                    _putchar(ch, buffer, &buffer_index, &char_count);
-                    break;
-                }
-                case 's':
-                {
-                    const char *str = va_arg(args, const char *);
+_putchar(ch, buffer, &buffer_index, &char_count);
+break;
+}
+case 's':
+{
+const char *str = va_arg(args, const char *);
 
-                    _puts(str, buffer, &buffer_index, &char_count);
-                    break;
-                }
-                case 'd':
-                case 'i':
-                {
-                    int value = va_arg(args, int);
+_puts(str, buffer, &buffer_index, &char_count);
+break;
+}
+case 'd':
+case 'i':
+{
+int value = va_arg(args, int);
 
-                    _int_to_str(value, buffer, &buffer_index, &char_count);
-                    break;
-                }
-                case '%':
+_int_to_str(value, buffer, &buffer_index, &char_count);
+break;
+}
+case '%':
 
-                    _putchar('%', buffer, &buffer_index, &char_count);
-                    break;
+_putchar('%', buffer, &buffer_index, &char_count);
+break;
 
-                default:
+default:
 
-                    _putchar(*format, buffer, &buffer_index, &char_count);
-                    break;
-            }
-        } 
-        else
-            _putchar(*format, buffer, &buffer_index, &char_count);
+_putchar(*format, buffer, &buffer_index, &char_count);
+break;
+}
+} 
+else
+_putchar(*format, buffer, &buffer_index, &char_count);
 
-        format++;
-    }
+format++;
+}
 
-    // Ensure null-terminated string
-    buffer[buffer_index] = '\0';
+// Ensure null-terminated string
+buffer[buffer_index] = '\0';
 
-    // Writing to stdout
-    write(STDOUT_FILENO, buffer, buffer_index);
+// Writing to stdout
+write(STDOUT_FILENO, buffer, buffer_index);
 
-    va_end(args);
+va_end(args);
 
-    return (char_count);
+return (char_count);
 }
